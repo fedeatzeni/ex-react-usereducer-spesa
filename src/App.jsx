fca016172969a56c83e14d35a3e97e21194ba36a
Fede <fede.atzeni372@gmail.com>
2025-06-02 13:18:25 +0200

    feat milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
 
   const products = [
@@ -7,17 +9,40 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const [addedProducts, setAddedProducts] = useState([]);
+  console.log(addedProducts)
+
+  function addToCart(product) {
+    if (!addedProducts.find(p => p.name === product.name)) {
+      setAddedProducts([...addedProducts, { ...product, quantity: 1 }])
+    }
+  }
+
   return (
     <>
       <h1>Prodotti</h1>
       <ul>
         {products.map((product, index) => (
           <li key={index}>
-            <div>{product.name}</div>
-            <div>{product.price + "€"}</div>
+            <p>{`${product.name} (${product.price.toFixed(2)}€)`}</p>
+
+            <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
           </li>
         ))}
       </ul>
+
+
+      {addedProducts.length > 0 && <>
+        <h1>Carrello</h1>
+        <ul>
+          {addedProducts.map((product, index) => (
+            <li key={index}>
+              <p>{`${product.quantity}x ${product.name} (${product.price.toFixed(2)}€)`}</p>
+            </li>
+          ))}
+        </ul>
+      </>}
+
     </>
   )
 }
